adfdfbe6840c4cb1939512b6038fa76cc1ed8d56
coffee-baiter <coffee.baiter@gmail.com>
2022-12-18 20:44:31 +0000

    wip

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('C:\\DATA\\PROJECTS\\REACT\\2022-12-18-react\\dev\\node_modules\\infima\\dist\\css\\default\\default.css'),
+  require('C:\\DATA\\PROJECTS\\REACT\\2022-12-18-react\\dev\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
+  require('C:\\DATA\\PROJECTS\\REACT\\2022-12-18-react\\dev\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
+  require('C:\\DATA\\PROJECTS\\REACT\\2022-12-18-react\\dev\\src\\css\\custom.css'),
+];
